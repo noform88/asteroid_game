963bad71c995f72792e96832d9a916c4d94e9a81
noform88 <manidanx@hotmail.com>
2024-11-02 11:23:32 +0800

    C2.8 added groups and containers.

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
     # limit how many frames we draw per second (FPS) using the .tick() method
     # track how much time has passed between frames (delta time)
 
+    updatable = pygame.sprite.Group() # we create a group called updatable so we can update all objects in 1 shot.
+    drawable = pygame.sprite.Group() #same, but for draw methods. empty groups atm.
+
+    Player.containers = (updatable, drawable) #all 'Player' class instances will be added to the groups its container mentions
+
     player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     dt = 0 # dt represent the amount of time that has passed since the last frame was drawn
 
@@ -18,12 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt) #updates player's latest action
+        for obj in updatable: #replace player with group 'updatable'. all class instances with update() will get executed cleanly
+            obj.update(dt)
 
         screen.fill((0,153,153)) #can also use color directly. eg "black"
 
-        player.draw(screen) #player appears on the interface
-
+        for obj in drawable: #using group "drawwable" instead of player. all onscreen objects drawn in 1 go.
+            obj.draw(screen)
         pygame.display.flip()   #use pygame function directly to clear the screen. it will know which screen to reset
 
         clock.tick(60) #will pause the game loop until 1/60th of a second has passed
