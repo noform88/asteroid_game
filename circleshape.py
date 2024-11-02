3a818ac71bfa4a02abbc644ef803f760f0f2033e
noform88 <manidanx@hotmail.com>
2024-11-02 16:11:23 +0800

    C3.2 added collision method and feature in-game

--- circleshape.py
+++ circleshape.py
@@ -3,20 +3,24 @@ import pygame
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):  #sprites: The base class for visible game objects
     def __init__(self, x, y, radius):
-        # we will be using this later
+    
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
 
-        self.position = pygame.Vector2(x, y) #pos
-        self.velocity = pygame.Vector2(0, 0) #velocity
-        self.radius = radius                 #radius
+        self.position = pygame.Vector2(x, y) 
+        self.velocity = pygame.Vector2(0, 0) 
+        self.radius = radius                 
 
     def draw(self, screen):
-        # sub-classes must override
+        pass
+    def update(self, dt):
         pass
 
-    def update(self, dt):
-        # sub-classes must override
-        pass
+    def collision(self, other): #self.position is pygame.Vector2(x, y).
+                                #it has a method of .distance_to()
+                                #it calculates the distance from centre of object1 to that of obj2
+        if self.position.distance_to(other.position) <= self.radius+ other.radius:
+            return True
+        return False
--- main.py
+++ main.py
@@ -3,39 +3,44 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
     clock = pygame.time.Clock() 
 
-    updatable = pygame.sprite.Group() # we create a group called updatable so we can update all objects in 1 shot.
+    updatable = pygame.sprite.Group() 
     drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group() 
+    asteroids = pygame.sprite.Group() #new group specifically for asteroids, for later use
 
-    Asteroid.containers = (updatable, drawable, asteroids)
-    AsteroidField.containers = (updatable)
-    Player.containers = (updatable, drawable) #all 'Player' class instances will be added to the groups its container mentions
+    Asteroid.containers = (updatable, drawable, asteroids) #all asteroids automatically added to these 3 groups at birth
+    AsteroidField.containers = (updatable) #astrofield only needs updating.
+    Player.containers = (updatable, drawable) 
 
     player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
-    asteroidfield = AsteroidField()
-    dt = 0 # dt represent the amount of time that has passed since the last frame was drawn
+    asteroidfield = AsteroidField() #initialize the field before the loop. loop just updates each cycle.
+    dt = 0 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        for obj in updatable: #replace player with group 'updatable'. all class instances with update() will get executed cleanly
+        for obj in updatable:
             obj.update(dt)
+        for asteroid in asteroids:  #collisions checks after updating movements but b4 being drawn onto screen
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit() #shuts game.
 
         screen.fill((0,153,153)) 
 
-        for obj in drawable: #using group "drawwable" instead of player. all onscreen objects drawn in 1 go.
+        for obj in drawable: 
             obj.draw(screen)
         pygame.display.flip()  
       
-        dt = clock.tick(60)/1000 #liimit framerate to 60fps
+        dt = clock.tick(60)/1000 #limit framerate to 60fps
 
 if __name__ == "__main__": 
     main()
